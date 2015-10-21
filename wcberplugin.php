04cdffb1a6bfd02f8c787f599c7bacdb2a889ba9
Dennis Ploetner <re@lloc.de>
2015-10-21 17:51:50 +0200

    Regular, context and excape functions added

---
+++ wcberplugin.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Plugin Name: WordCamp Berlin 2015 Test Plugin
+ * Description: Example plugin for my slots at the WordCamp Berlin 2015
+ * Author:      Dennis Ploetner
+ * Author URI:  https://profiles.wordpress.org/realloc
+ * Text Domain: wcbertheme
+ */
+
+/**
+ * Load plugin textdomain.
+ */
+add_action( 'plugins_loaded', function () {
+	load_plugin_textdomain( 'wcberplugin', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+} );
+
+/**
+ * do_action( 'wcberplugin/regular' );
+ */
+add_action( 'wcberplugin/regular', function () {
+	echo '<h2>', __( 'Hello, dear user!', 'wcberplugin' ), '</h2>';
+
+	echo '<p>';
+	_e( 'Howdy!', 'wcberplugin' );
+	echo '</p>';
+
+	$stars = 1;
+	$str   = _n( 'One star', 'More stars', $stars, 'wcberplugin' );
+	echo $str; // One star
+
+	$stars = 3;
+	printf( _n( '%d star', '%d stars', $stars, 'wcberplugin' ), $stars ); // 3 stars
+} );
+
+/**
+ * do_action( 'wcberplugin/with_context' );
+ */
+add_action( 'wcberplugin/with_context', function () {
+	echo '<h2>', _x( 'Hello, dear user!', 'With Context', 'wcberplugin' ), '</h2>';
+
+	echo '<p>';
+	_ex( 'Howdy!', 'With Context', 'wcberplugin' );
+	echo '</p>';
+
+	$stars = 1;
+	$str   = _nx( 'One star', 'More stars', $stars, 'With Context', 'wcberplugin' );
+	echo $str; // One star
+
+	$stars = 3;
+	printf( _nx( '%d star', '%d stars', $stars, 'With Context', 'wcberplugin' ), $stars ); // 3 stars
+} );
+
+/**
+ * do_action( 'wcberplugin/esc_text' );
+ */
+add_action( 'wcberplugin/esc_text', function () {
+	printf( '<h2 title="%s">%s</h2>', esc_attr__( 'Hello, dear user!', 'wpberplugin' ), esc_html__( 'Hello, dear user!', 'wpberplugin' ) );
+
+	printf( '<h2 title="%s">%s</h2>', esc_attr__( 'Hello, dear user!', 'wpberplugin' ), esc_html__( 'Hello, dear user!', 'wpberplugin' ) );
+
+} );
+
